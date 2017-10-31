101ea2e8b2ea8cb4e85bc1ef86a925af073f8c9b
Derek <dphtran501@yahoo.com>
2017-10-31 16:29:54 -0700

    implemented onShake in MagicAnswerActivity.java

--- app/src/main/java/edu/orangecoastcollege/cs273/magicanswer/MagicAnswerActivity.java
+++ app/src/main/java/edu/orangecoastcollege/cs273/magicanswer/MagicAnswerActivity.java
@@ -1,16 +1,26 @@
 package edu.orangecoastcollege.cs273.magicanswer;
 
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
 
-public class MagicAnswerActivity extends AppCompatActivity {
+public class MagicAnswerActivity extends AppCompatActivity
+{
 
     MagicAnswer magicAnswer;
     private TextView answerTextView;
 
+    private SensorManager mSensorManager;
+    private Sensor accelerometer;
+    // Create a reference to our ShakeDetector
+    private ShakeDetector mShakeDetector;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_magic_answer);
 
@@ -18,9 +28,33 @@ public class MagicAnswerActivity extends AppCompatActivity {
         answerTextView = (TextView) findViewById(R.id.answerTextView);
 
         // TASK 2: CREATE A NEW MAGIC ANSWER OBJECT
+        magicAnswer = new MagicAnswer(this);
 
         // TASK 3: REGISTER THE SENSOR MANAGER AND SETUP THE SHAKE DETECTION
+        mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        accelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
 
+        mShakeDetector = new ShakeDetector(new ShakeDetector.OnShakeListener()
+        {
+            @Override
+            public void onShake()
+            {
+                displayMagicAnswer();
+            }
+        });
+
+    }
+
+    public void displayMagicAnswer()
+    {
+        String randomAnswer = magicAnswer.getRandomAnswer();
+        answerTextView.setText(randomAnswer);
+    }
+
+    protected void onResume()
+    {
+        super.onResume();
+        mSensorManager.registerListener(mShakeDetector, accelerometer, SensorManager.SENSOR_DELAY_UI);
     }
 
 
